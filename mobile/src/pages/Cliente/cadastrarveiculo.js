5c010bb45b0e7cffc94500f59289f73151f338a0
Thiago Freitas <tfreitas1983@gmail.com>
2022-06-21 22:51:46 -0300

    Mostrando modelos

--- mobile/src/pages/Cliente/cadastrarveiculo.js
+++ mobile/src/pages/Cliente/cadastrarveiculo.js
@@ -5,7 +5,6 @@ import Entypo from 'react-native-vector-icons/Entypo';
 import { dataMask } from '../../components/masks';
 import axios from 'axios';
 import moment from 'moment';
-import SelectDropdown from 'react-native-select-dropdown';
 import { AutocompleteDropdown } from 'react-native-autocomplete-dropdown';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -106,28 +105,26 @@ const styles = StyleSheet.create({
 
 export default function CadastrarVeiculos  ({ navigation }) {
 
-  //const [marcas, setMarcas] = useState(null);
- // const [modelos, setModelos] = useState([]);
- let modelos = [];
+
   const [anos, setAnos] = useState([]);
   const [dados, setDados] = useState([]);
 
   const [loading, setLoading] = useState(false)
   const [loading2, setLoading2] = useState(false)
+  const [loading3, setLoading3] = useState(false)
   const [suggestionsList, setSuggestionsList] = useState(null)
   const [selectedItem, setSelectedItem] = useState(null)
   const dropdownController = useRef(null)
   const searchRef = useRef(null);
   const searchRef2 = useRef(null);
+  const searchRef3 = useRef(null);
   const kmRef = useRef('');
   const chassiRef = useRef('');
   const aquisicaoRef = useRef('');
   const gnvRef = useRef('');
 
   const [renavam, setRenavam] = useState('');
-  const [marca, setMarca] = useState('');
-  let idMarca = null;//, setIdMarca] = useState('');
-  const [modelo, setModelo] = useState('');
+  const [modelo, setModelo] = useState([]);
   const [idModelo, setIdModelo] = useState('');
   const [ano, setAno] = useState([]);
   const [idAno, setIdAno] = useState('');
@@ -136,8 +133,8 @@ export default function CadastrarVeiculos  ({ navigation }) {
   const [km, setKm] = useState('');
   const [chassi, setChassi] = useState('');
   const [gnv, setGnv] = useState('');
-  let listamodelos = null;
-  let mostrarmodelos = null;
+  let idMarca = null;  
+  let modelos = {};
 
   const marcas = [{"id": "1", "title": "Acura"}, {"id": "2", "title": "Agrale"}, {"id": "3", "title": "Alfa Romeo"}, {"id": "4", "title": "AM Gen"}, {"id": "5", "title": "Asia Motors"}, {"id": "189", "title": "ASTON MARTIN"}, {"id": "6", "title": "Audi"}, {"id": "207", "title": "Baby"}, {"id": "7", "title": "BMW"}, {"id": "8", "title": "BRM"}, {"id": "123", "title": "Bugre"}, {"id": "238", "title": "BYD"}, {"id": "236", "title": "CAB Motors"}, {"id": "10", "title": "Cadillac"}, {"id": "161", "title": "Caoa Chery"}, {"id": "11", "title": "CBT Jipe"}, {"id": "136", "title": "CHANA"}, {"id": "182", "title": "CHANGAN"}, {"id": "12", "title": "Chrysler"}, {"id": "13", "title": "Citroën"}, {"id": "14", "title": "Cross Lander"}, {"id": "15", "title": "Daewoo"}, {"id": "16", "title": "Daihatsu"}, {"id": "17", "title": "Dodge"}, {"id": "147", "title": "EFFA"}, {"id": "18", "title": "Engesa"}, {"id": "19", "title": "Envemo"}, {"id": "20", "title": "Ferrari"}, {"id": "21", "title": "Fiat"}, {"id": "149", "title": "Fibravan"}, {"id": "22", "title": "Ford"}, {"id": "190", "title": "FOTON"}, {"id": "170", "title": "Fyber"}, {"id": "199", "title": "GEELY"}, {"id": "23", "title": "GM - Chevrolet"}, {"id": "153", "title": "GREAT WALL"}, {"id": "24", "title": "Gurgel"}, {"id": "152", "title": "HAFEI"}, {"id": "214", "title": "HITECH ELECTRIC"}, {"id": "25", "title": "Honda"}, {"id": "26", "title": "Hyundai"}, {"id": "27", "title": "Isuzu"}, {"id": "208", "title": "IVECO"}, {"id": "177", "title": "JAC"}, {"id": "28", "title": "Jaguar"}, {"id": "29", "title": "Jeep"}, {"id": "154", "title": "JINBEI"}, {"id": "30", "title": "JPX"}, {"id": "31", "title": "Kia Motors"}, {"id": "32", "title": "Lada"}, {"id": "171", "title": "LAMBORGHINI"}, {"id": "33", "title": "Land Rover"}, {"id": "34", "title": "Lexus"}, {"id": "168", "title": "LIFAN"}, {"id": "127", "title": "LOBINI"}, {"id": "35", "title": "Lotus"}, {"id": "140", "title": "Mahindra"}, {"id": "36", "title": "Maserati"}, {"id": "37", "title": "Matra"}, {"id": "38", "title": "Mazda"}, {"id": "211", "title": "Mclaren"}, {"id": "39", "title": "Mercedes-Benz"}, {"id": "40", "title": "Mercury"}, {"id": "167", "title": "MG"}, {"id": "156", "title": "MINI"}, {"id": "41", "title": "Mitsubishi"}, {"id": "42", "title": "Miura"}, {"id": "43", "title": "Nissan"}, {"id": "44", "title": "Peugeot"}, {"id": "45", "title": "Plymouth"}, {"id": "46", "title": "Pontiac"}, {"id": "47", "title": "Porsche"}, {"id": "185", "title": "RAM"}, {"id": "186", "title": "RELY"}, {"id": "48", "title": "Renault"}, {"id": "195", "title": "Rolls-Royce"}, {"id": "49", "title": "Rover"}, {"id": "50", "title": "Saab"}, {"id": "51", "title": "Saturn"}, {"id": "52", "title": "Seat"}, {"id": "183", "title": "SHINERAY"}, {"id": "157", "title": "smart"}, {"id": "125", "title": "SSANGYONG"}, {"id": "54", "title": "Subaru"}, {"id": "55", "title": "Suzuki"}, {"id": "165", "title": "TAC"}, {"id": "56", "title": "Toyota"}, {"id": "57", "title": "Troller"}, {"id": "58", "title": "Volvo"}, {"id": "59", "title": "VW - VolksWagen"}, {"id": "163", "title": "Wake"}, {"id": "120", "title": "Walk"}];
   
@@ -175,14 +172,26 @@ export default function CadastrarVeiculos  ({ navigation }) {
       resp2 = await resp2;
       
       if (modelos) {
-        
-         listamodelos = modelos.modelos.map(itens => ( {             
-           id: itens.codigo,
-           title: itens.nome
-         }));   
-         console.log('listamodelos', listamodelos);
+        setLoading(false)
+        setLoading2(true);
+        setModelo(modelos.modelos.map(itens =>  {  
+          return {           
+          id: ''+itens.codigo+'',
+          title: itens.nome
+        }}));  
+         console.log('listamodelos', modelo);
+         setLoading2(false)
+
+         setAnos(modelo.map(itens =>  {  
+          return {           
+          id: ''+itens.codigo+'',
+          title: itens.nome
+        }}));  
+
+        console.log('anos', anos);
       } 
-      setLoading(false)
+         
+
     } else {
       idMarca = item.id;
       let resp2 = await axios.get(`https://parallelum.com.br/fipe/api/v1/carros/marcas/${idMarca}/modelos`) 
@@ -195,97 +204,34 @@ export default function CadastrarVeiculos  ({ navigation }) {
       resp2 = await resp2;
       
       if (modelos) {
-        
-         listamodelos = modelos.modelos.map(itens => ( {             
-           id: itens.codigo,
+        setLoading(false)
+        setLoading2(true);
+        setModelo(modelos.modelos.map(itens =>  {  
+           return {           
+           id: ''+itens.codigo+'',
            title: itens.nome
-         }));   
-         console.log('listamodelos', listamodelos);
-      } 
-      setLoading(false)
+         }}));   
+         console.log('listamodeloselse', modelo);
 
-    }  
-    
+         setLoading2(false);
 
-    setTimeout(() => {
-      if (listamodelos.length > 0) {
-        console.log('ok');
+         setAnos(modelo.map(itens =>  {  
+          return {           
+          id: ''+itens.codigo+'',
+          title: itens.nome
+        }}));  
 
-        mostrarmodelos = <View>
-        <AutocompleteDropdown
-        ref={searchRef2}          
-        clearOnFocus={false}
-        closeOnBlur={true}
-        closeOnSubmit={false}
-        dataSet={listamodelos}
-        onSelectItem={ item => PegaModelo(item) }
-        suggestionsListMaxHeight={Dimensions.get('window').height * 0.4}
-        onClear={onClearPress}
-        loading={loading2}
-        useFilter={true}
-        textInputProps={{
-          placeholder: 'Digite 3 ou mais caracteres',
-          autoCorrect: false,
-          autoCapitalize: 'none',
-          placeholderTextColor: '#fff',
-          style: {
-            borderRadius: 15,
-            backgroundColor: '#fd8900',
-            color: '#fff',
-            paddingLeft: 18,
-            width: Dimensions.get('window').width*0.6
-          },
-        }}
-        rightButtonsContainerStyle={{
-          right: 8,
-          height: 30,
-          alignSelf: 'center',
-        }}
-        inputContainerStyle={{
-          backgroundColor: '#fd8900',
-          borderRadius: 25,
-        }}
-        suggestionsListContainerStyle={{ backgroundColor: '#fd8900' }}
-        containerStyle={{ flexGrow: 1, flexShrink: 1 }}
-        renderItem={(items, text) => <Text style={{ color: '#fff', padding: 15 }}>{items.title}</Text>}
-        ChevronIconComponent={<Feather name="chevron-down" size={20} color="#fff" />}
-        ClearIconComponent={<Feather name="x-circle" size={20} color="#fff" />}
-        inputHeight={50}
-        showChevron={true}
-        /> </View>
-      } else {
-        mostrarmodelos = <Text>Sem resposta</Text>
-      }  
-    }, 5000);
-    
- }
-
- if (listamodelos) {
-   mostrarmodelos = <SelectDropdown
-   data={listamodelos.map(item => {return item.title})}
-   defaultButtonText="Selecione o modelo"              
-   buttonStyle={styles.bordado}
-   onSelect={(selectedItem, index) => {
-     console.log(selectedItem, index)
-     setSexo(selectedItem)
-   }}
-   buttonTextAfterSelection={(selectedItem, index) => {
-     // text represented after item is selected
-     // if data array is an array of objects then return selectedItem.property to render after item is selected
-     return selectedItem
-   }}
-   rowTextForSelection={(item, index) => {
-     // text represented for each item in dropdown
-     // if data array is an array of objects then return item.property to represent item in dropdown
-     return item
-   }}
- />
- } else {
-   mostrarmodelos = <Text>Aguardando...</Text>
+        console.log('anos', anos);
+        
+      }
+    }    
  }
 
  function PegaModelo (item) {
  }
+
+ function PegaAno (item) {
+}
  
 
   async function buscar () {
@@ -314,12 +260,23 @@ export default function CadastrarVeiculos  ({ navigation }) {
         }
   }
 
-
-
   const onClearPress = useCallback(() => {
     setSuggestionsList(null)
     idMarca = null;
     modelos = [];
+    setModelo([]);
+    setAnos([]);
+  }, [])
+
+  const onClearPress2 = useCallback(() => {
+    setSuggestionsList(null)
+    setModelo([]);
+    setAnos([]);
+  }, [])
+
+  const onClearPress3 = useCallback(() => {
+    setSuggestionsList(null)
+    setAnos([]);
   }, [])
 
   const onOpenSuggestionsList = useCallback(isOpened => {}, [])
@@ -353,6 +310,7 @@ export default function CadastrarVeiculos  ({ navigation }) {
            você terá o histórico. Caso contrário você pode cadastrá-lo abaixo
           </Text>
           
+         
          
           <AutocompleteDropdown
           ref={searchRef}          
@@ -396,11 +354,95 @@ export default function CadastrarVeiculos  ({ navigation }) {
           showChevron={true}
           />
 
-          
+          <Text style={styles.opcoes}> Modelos</Text>
+          <AutocompleteDropdown
+          ref={searchRef2}          
+          clearOnFocus={false}
+          closeOnBlur={true}
+          closeOnSubmit={false}
+          dataSet={modelo}
+          onSelectItem={ item => PegaModelo(item) }
+          suggestionsListMaxHeight={Dimensions.get('window').height * 0.4}
+          onClear={onClearPress2}
+          loading={loading2}
+          useFilter={true}
+          textInputProps={{
+            placeholder: 'Digite 3 ou mais caracteres',
+            autoCorrect: false,
+            autoCapitalize: 'none',
+            placeholderTextColor: '#fff',
+            style: {
+              borderRadius: 15,
+              backgroundColor: '#fd8900',
+              color: '#fff',
+              paddingLeft: 18,
+              width: Dimensions.get('window').width*0.6
+            },
+          }}
+          rightButtonsContainerStyle={{
+            right: 8,
+            height: 30,
+            alignSelf: 'center',
+          }}
+          inputContainerStyle={{
+            backgroundColor: '#fd8900',
+            borderRadius: 25,
+          }}
+          suggestionsListContainerStyle={{ backgroundColor: '#fd8900' }}
+          containerStyle={{ flexGrow: 1, flexShrink: 1 }}
+          renderItem={(item, text) => <Text style={{ color: '#fff', padding: 15 }}>{item.title}</Text>}
+          ChevronIconComponent={<Feather name="chevron-down" size={20} color="#fff" />}
+          ClearIconComponent={<Feather name="x-circle" size={20} color="#fff" />}
+          inputHeight={50}
+          showChevron={true}
+          />
+
+        <Text style={styles.opcoes}> Anos</Text>
+          <AutocompleteDropdown
+          ref={searchRef3}          
+          clearOnFocus={false}
+          closeOnBlur={true}
+          closeOnSubmit={false}
+          dataSet={anos}
+          onSelectItem={ item => PegaModelo(item) }
+          suggestionsListMaxHeight={Dimensions.get('window').height * 0.4}
+          onClear={onClearPress3}
+          loading={loading3}
+          useFilter={true}
+          textInputProps={{
+            placeholder: 'Digite 3 ou mais caracteres',
+            autoCorrect: false,
+            autoCapitalize: 'none',
+            placeholderTextColor: '#fff',
+            style: {
+              borderRadius: 15,
+              backgroundColor: '#fd8900',
+              color: '#fff',
+              paddingLeft: 18,
+              width: Dimensions.get('window').width*0.6
+            },
+          }}
+          rightButtonsContainerStyle={{
+            right: 8,
+            height: 30,
+            alignSelf: 'center',
+          }}
+          inputContainerStyle={{
+            backgroundColor: '#fd8900',
+            borderRadius: 25,
+          }}
+          suggestionsListContainerStyle={{ backgroundColor: '#fd8900' }}
+          containerStyle={{ flexGrow: 1, flexShrink: 1 }}
+          renderItem={(item, text) => <Text style={{ color: '#fff', padding: 15 }}>{item.title}</Text>}
+          ChevronIconComponent={<Feather name="chevron-down" size={20} color="#fff" />}
+          ClearIconComponent={<Feather name="x-circle" size={20} color="#fff" />}
+          inputHeight={50}
+          showChevron={true}
+          />
+  
 
          <ScrollView> 
-
-           {mostrarmodelos}  
+        
 
           <Input       
           keyboardType="default"         
